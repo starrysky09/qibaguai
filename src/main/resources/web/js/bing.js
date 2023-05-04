66a88fad3619c9068a3b4d75396ec94a060f1213
starrysky09 <130130780+starrysky09@users.noreply.github.com>
2023-05-04 11:20:46 +0800

    Update bing.js

--- src/main/resources/web/js/bing.js
+++ src/main/resources/web/js/bing.js
@@ -1252,7 +1252,7 @@ class TitleManager{
 	 * */
 	restart(){
 		this.setSubtitleText('想发什么呢？让我帮你！');
-		document.title = 'NewBing:人力资源';
+		document.title = 'NewBingWeb:人力资源';
 	}
 	/**
 	 * 正在创建聊天时
